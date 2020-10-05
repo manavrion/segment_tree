b30db0c66a7b27552dd8c1c8125e8af386a5e34d
Ruslan Manaev <manavrion@gmail.com>
2020-10-05 16:35:57 +0500

    Add ccache

---
+++ cmake/ccache.cmake
@@ -0,0 +1,11 @@
+# CCache
+
+message("-- Detecting CCache")
+find_program(CCACHE_PROGRAM ccache)
+
+if(CCACHE_PROGRAM)
+  set(CMAKE_CXX_COMPILER_LAUNCHER "${CCACHE_PROGRAM}")
+  message("-- Detecting CCache - done")
+else()
+  message("-- Detecting CCache - failed")
+endif()
